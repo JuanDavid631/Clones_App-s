b1f98427f93d5fad806fafd12a4283a0ad9efb72
Juan David Montoya Lesmes <99360486+JuanDavid631@users.noreply.github.com>
2024-02-26 18:18:55 -0500

    Imports File

--- To Do - App/js/main.js
+++ To Do - App/js/main.js
@@ -1,4 +1,7 @@
 import { addTask } from "../components/addTask";
+import { readTasks } from "./components/readTasks.js";
 
 const btn = document.querySelector("[data-form-btn]");
 btn.addEventListener("click", addTask);
+
+readTasks();
