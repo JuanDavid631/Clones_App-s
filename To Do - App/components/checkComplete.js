1ae3ce1608173913215cfd5d877af6dde656ad71
Juan David Montoya Lesmes <99360486+JuanDavid631@users.noreply.github.com>
2024-02-26 17:50:55 -0500

    Complete task

---
+++ To Do - App/components/checkComplete.js
@@ -0,0 +1,23 @@
+export const checkComplete = (id) => {
+  const i = document.createElement("i");
+  i.classList.add("far", "fa-check-square", "icon");
+  i.addEventListener("click", (event) => completeTask(event, id));
+  return i;
+};
+
+const completeTask = (event, id) => {
+  const element = event.target;
+  element.classList.toggle("fas");
+  element.classList.toggle("completeIcon");
+  element.classList.toggle("far");
+  const tasks = JSON.parse(localStorage.getItem("tasks"));
+  const index = tasks.findIndex((item) => item.id === id);
+
+  tasks[index].complete = !tasks[index].complete;
+
+  if (tasks[index].complete == true) {
+    Swal.fire("Genial!", "Has completado una tarea", "success");
+  }
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
